ffee52372da86801d2e59d7e0f108e90ec927fc6
Ravi Kazi Karmacharya <ravidaai@gmail.com>
2022-06-23 18:00:43 +0545

    vue animation practice

---
+++ chapter-3/src/views/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="home">
+    <Toast v-if="showToast" />
+    <Todos @badValue="triggerToast" />
+    <transition name="fade">
+      <div v-if="showP">hello, ninjas</div>
+    </transition>
+    <button @click="showP = !showP">toggle</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Toast from '../components/Toast'
+import Todos from '../components/Todos'
+
+export default {
+  components: { Toast, Todos },
+  setup() {
+    const showToast = ref(false)
+    const showP = ref(false)
+
+    const triggerToast = () => {
+      showToast.value = true;
+      setTimeout(() => showToast.value = false, 3000)
+    }
+
+    return { showToast, triggerToast, showP }
+  }
+}
+</script>
+
+<style>
+  .fade-enter-from {
+    opacity: 0;
+  }
+  .fade-enter-to {
+    opacity: 1;
+  }
+  .fade-enter-active {
+    transition: all 2s ease;
+  }
+  .fade-leave-from {
+    opacity: 1;
+  }
+  .fade-leave-to {
+    opacity: 0;
+  }
+  .fade-leave-active {
+    transition: all 2s ease;
+  }
+</style>
---
+++ chapter-4/src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home">
+    <transition name="toast">
+      <Toast v-if="showToast" />
+    </transition>
+    <Todos @badValue="triggerToast" />
+    <button @click="showP = !showP">toggle</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Toast from '../components/Toast'
+import Todos from '../components/Todos'
+
+export default {
+  components: { Toast, Todos },
+  setup() {
+    const showToast = ref(false)
+
+    const triggerToast = () => {
+      showToast.value = true;
+      setTimeout(() => showToast.value = false, 3000)
+    }
+
+    return { showToast, triggerToast }
+  }
+}
+</script>
+
+<style>
+  /* enter transitions */
+  .toast-enter-from {
+    opacity: 0;
+    transform: translateY(-60px);
+  }
+  /* .toast-enter-to {
+    opacity: 1;
+    transform: translateY(0);
+  } */
+  .toast-enter-active {
+    transition: all 0.3s ease;
+  }
+  /* leave transitions */
+  /* .toast-leave-from {
+    opacity: 1;
+    transform: translateY(0);
+  } */
+  .toast-leave-to {
+    opacity: 0;
+    transform: translateY(-60px);
+  }
+  .toast-leave-active {
+    transition: all 0.3s ease;
+  }
+</style>
